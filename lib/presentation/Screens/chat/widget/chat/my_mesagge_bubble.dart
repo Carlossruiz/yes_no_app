f8827f2944241e40ec361b9196e0da50f790db05
carlossruiz <cruiara0501@g.educaand.es>
2023-10-02 14:36:54 +0200

    los dos mensajes con gif

--- lib/presentation/Screens/chat/widget/chat/my_mesagge_bubble.dart
+++ lib/presentation/Screens/chat/widget/chat/my_mesagge_bubble.dart
@@ -23,7 +23,20 @@ class MyMessageBubble extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 10),
+
+        _ImageBubble()
+
       ],
     );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network("https://yesno.wtf/assets/yes/13-c3082a998e7758be8e582276f35d1336.gif"));
   }
 }
